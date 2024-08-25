529abf75638fb24ac5540799b967a44ea401d87c
Vivek Suyal <123635562+viveksuyal37@users.noreply.github.com>
2024-08-25 22:27:25 +0530

    Update rootSlice.ts

--- apps/web/redux/apiSlices/rootSlice.ts
+++ apps/web/redux/apiSlices/rootSlice.ts
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 
 export const rootApiSlice = createApi({
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3002' }),
+  baseQuery: fetchBaseQuery({ baseUrl: 'https://rq-analytics.onrender.com' }),
   endpoints: () => ({}),
 });
